43c0ceb9a8d760ef2b3dbde7ee0ba19f0cdccf4b
Saad bin Abid <saad.abid@crowdbotics.com>
2023-05-29 15:10:10 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "qa-test-41066",
-  url: "https://qa-test-41066.botics.co",
-  api: "https://qa-test-41066.botics.co/api/v1"
+  "name": "qa-test-41066",
+  "url": "https://qa-test-41066.botics.co",
+  "api": "https://qa-test-41066.botics.co/api/v1",
+  "initialRoute": "Untitled1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,3 +1,5 @@
+import { Pressable } from "react-native";
+import { Text } from "react-native";
 import { ImageBackground } from "react-native";
 import React from "react";
 import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
@@ -9,7 +11,7 @@ const Untitled1 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}><ImageBackground style={styles.tSijTtLy} source={require("./logo of qa.png")} resizeMode="cover"><ImageBackground style={styles.clKiUQON} source={require("./QA_PIC.jpg")} resizeMode="cover"></ImageBackground><View style={styles.XXImutqw}></View></ImageBackground></ScrollView>
+    }}><ImageBackground style={styles.tSijTtLy} source={require("./logo of qa.png")} resizeMode="cover"><ImageBackground style={styles.clKiUQON} source={require("./QA_PIC.jpg")} resizeMode="cover"></ImageBackground><View style={styles.XXImutqw}><Pressable><Text style={styles.hNuYJHok}>{"Contact Us"}</Text></Pressable></View><Pressable><View style={styles.YrbzOzAo}><Text style={styles.bTLIKFIR}>{"About Us"}</Text></View></Pressable><Pressable><View style={styles.lFgTUvNB}><Text style={styles.lDFIxoMH}>{"Terms and Conditions"}</Text></View></Pressable><Pressable><View style={styles.OrDyGryD}><Text style={styles.UsAwfPBD}>{"Privacy Policy"}</Text></View></Pressable></ImageBackground></ScrollView>
     </SafeAreaView>;
 };
 
@@ -31,12 +33,90 @@ const styles = StyleSheet.create({
   XXImutqw: {
     height: 30,
     width: 80,
-    backgroundColor: "#E4E4E4",
+    backgroundColor: "#3dc3c3",
     borderRadius: 17,
     color: "#777777",
     position: "absolute",
     top: 450,
     left: 135
+  },
+  YrbzOzAo: {
+    height: 30,
+    width: 80,
+    backgroundColor: "#3DC3C3",
+    borderRadius: 17,
+    color: "#777777",
+    position: "absolute",
+    top: 485,
+    left: 135
+  },
+  lFgTUvNB: {
+    height: 31,
+    width: 153,
+    backgroundColor: "#338bdc",
+    borderRadius: 17,
+    color: "#777777",
+    position: "absolute",
+    top: 536,
+    left: 98
+  },
+  OrDyGryD: {
+    height: 30,
+    width: 153,
+    backgroundColor: "#338BDC",
+    borderRadius: 17,
+    color: "#777777",
+    position: "absolute",
+    top: 577,
+    left: 98
+  },
+  hNuYJHok: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    position: "absolute",
+    top: 8,
+    left: 5,
+    fontWeight: "700",
+    color: "#ffffff"
+  },
+  bTLIKFIR: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    position: "absolute",
+    top: 8,
+    color: "#fffcfc",
+    fontWeight: "700",
+    left: 10
+  },
+  lDFIxoMH: {
+    width: 148,
+    height: 31,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    position: "absolute",
+    top: 7,
+    left: 10,
+    color: "#ffffff",
+    fontWeight: "700"
+  },
+  UsAwfPBD: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    position: "absolute",
+    top: 8,
+    fontWeight: "700",
+    color: "#fffdfd",
+    left: 32
   }
 });
 export default Untitled1;
