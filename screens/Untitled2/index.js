c819918e2250f72df32ae89d165c604d5bdbc641
Saad bin Abid <saad.abid@crowdbotics.com>
2023-05-29 15:14:09 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,3 +1,5 @@
+import { Text } from "react-native";
+import { ImageBackground } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
@@ -8,13 +10,29 @@ const Untitled2 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><ImageBackground style={styles.gxWwKAEU} source={require("./contact us .png")} resizeMode="cover"><Text style={styles.WvVWspeQ}>{"Contact Us"}</Text></ImageBackground></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  gxWwKAEU: {
+    width: 341,
+    height: 611
+  },
+  WvVWspeQ: {
+    width: 172,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 28,
+    borderRadius: 0,
+    color: "#ffffff",
+    fontWeight: "700",
+    position: "absolute",
+    top: 119,
+    left: 84
   }
 });
 export default Untitled2;
